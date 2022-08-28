f73a4199e2d733eb0a5fb9e338c6dab4b6d9262c
Douglas Lopes <douglasdmlopes@gmail.com>
2022-08-28 17:39:15 -0300

    Fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,10 @@ import { useLocation } from 'react-router-dom'
 
 import Message from "../layouts/Message"
 
+import styles from './Project.module.css'
+import Container from '../layouts/Container'
+import LinkButton from '../layouts/LinkButton'
+
 export default function Projects() {
 
   const location = useLocation()
@@ -13,9 +17,15 @@ export default function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to='/new-project' text='Criar Projeto'/>
+      </div>
       {message && <Message type='success' msg={message}/>}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
