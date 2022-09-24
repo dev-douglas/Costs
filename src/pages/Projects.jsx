8a3c413be9c1dcef64503581ba0acc06f421771f
Douglas Lopes <douglasdmlopes@gmail.com>
2022-09-24 20:47:40 -0300

    Remoção de projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,9 +13,10 @@ export default function Projects() {
 
   const [projects, setProjects] = useState([])
   const [removeLoading, setLoading] = useState(false)
+  const [projectMessage, setProjectMessage] = useState('')
 
   const location = useLocation()
-  console.log(location)
+  
   let message = ''
 
   if(location.state) {
@@ -39,6 +40,20 @@ export default function Projects() {
     }, 100)
   }, [])
 
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then( data => {
+      setProjects(projects.filter( (project) => project.id != id))
+      setProjectMessage('Projeto removido com sucesso!')
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -46,6 +61,7 @@ export default function Projects() {
         <LinkButton to='/new-project' text='Criar Projeto'/>
       </div>
       {message && <Message type='success' msg={message}/>}
+      {projectMessage && <Message type='success' msg={projectMessage}/>}
       <Container customClass="start">
         {
           projects.length > 0 && projects.map((project) => (
@@ -55,6 +71,7 @@ export default function Projects() {
               budget={project.budget}
               category={project.category.name}
               key={project.id}
+              handleRemove={removeProject}
             />
           ))
         }
